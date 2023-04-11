1b10c34bd9b38ae8bdddb76d9b862fb360d23e60
20221401_이채영 <127109886+20221401@users.noreply.github.com>
2023-04-11 23:01:28 +0900

    Add files via upload

---
+++ 고급C++ 06_2주차.cpp
@@ -0,0 +1,16 @@
+﻿// 고급C++ 06_2주차.cpp
+//Q.17
+#include<iostream>
+using namespace std;
+
+int& func_ref(int s[], int idx) {
+	return s[idx];
+}
+int main() {
+	int arr[3] = { 1, 2, 3 };
+	func_ref(arr, 1) = 5;
+	int& ref = func_ref(arr, 2);
+	ref = 9;
+	return 0;
+}
+//디버깅 결과: arr[2]=ref=9로 같음
